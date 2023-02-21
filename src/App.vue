2c6b833d1c1da4e47c0d5fd76e66652338170117
zhenglr <zhenglr@rd.netease.com>
2023-02-21 11:33:22 +0800

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
+
+</script>
+
+<template>
+  <el-button>我是 ElButton</el-button>
+  <el-alert  type="success" >adadasdadad<a>12313</a></el-alert>
+  <header>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo.svg"
+      width="125"
+      height="125"
+    />
+
+    <div>
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
