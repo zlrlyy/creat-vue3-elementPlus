a6d397be9c6e578d2ad7846618dd853b4b0ab562
zhenglr <zhenglr@rd.netease.com>
2023-02-27 11:16:14 +0800

    feat: 删除初始页面

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
-<script setup lang="ts">
-import TheWelcome from "../components/TheWelcome.vue";
+<script lang="ts">
+import {defineComponent} from 'vue';
+import HelloWorld from "../components/HelloWorld.vue";
+
+export default defineComponent({})
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <HelloWorld msg="123123" />
   </main>
 </template>
